25f48b97186bb72e55e5c3a36b135599a0ca1f16
liamm <gdschanu.lamlt2102@gmail.com>
2023-11-15 01:00:59 +0700

    config

---
+++ README.md
@@ -0,0 +1,10 @@
+1. npm install
+
+2. npm start (nodemon installed)
+
+3. Remember to remove .gitignore, README.md and node_modules before submit
+
+4. Submit a single zip file named in the format: "<full_name>_<student_id>.zip"
+
+This zip file contains a single folder which is your project’s root folder. You should use
+the format "wpr<student_id>" to name your project folder
---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "wpr",
+//   password: "fit2023",
+//   database: "wpr2023",
+// });
+
+app.set("view engine", "ejs");
+
+// stattic files inside public folder
+app.use(express.static("public"));
+
+// Example:
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.use(express.static("public"));
+app.listen(8000);
