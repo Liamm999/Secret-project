41b4a9d608070c8b166a1415f15e47e89e9f344b
liamm <gdschanu.femember@gmail.com>
2023-11-22 15:46:57 +0700

    restructoring

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const { createMySQLConnection } = require("./dbconn");
-const { route } = require("./routes");
 
 app.set("view engine", "ejs");
 
@@ -27,8 +26,6 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-route(app);
-
 /*
 =================================================================================================================================
 
@@ -41,6 +38,53 @@ const getUserIdFromReq = (req) => {
   const userId = req.cookies.userId;
   return userId;
 };
+
+/*
+=================================================================================================================================
+
+                                    SINGIN PAGE
+
+=================================================================================================================================
+*/
+
+app.get("/", (req, res) => {
+  const userId = req.cookies.userId;
+
+  if (userId) {
+    // change redirect to "/inbox" when finishe "inbox" page
+    return res.redirect("/outbox");
+  } else {
+    res.render("signin", {
+      title: "Sign in page",
+    });
+  }
+});
+
+// Sign in API endpoint
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const sql = `SELECT * FROM wpr2023.user WHERE email = ? AND password = ?`;
+
+  const conn = await createMySQLConnection();
+
+  try {
+    const [results] = await conn.execute(sql, [email, password]);
+
+    if (results.length > 0) {
+      const user = results[0];
+      res.cookie("userId", user.id, {
+        httpOnly: true,
+      });
+      return res.status(200).json(user);
+    } else {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  } finally {
+    conn.end();
+  }
+});
 
 /*
 =================================================================================================================================
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const signinRouter = require("./signin.route");
-
-function route(app) {
-  app.use("/", signinRouter);
-}
-
-module.exports = { route };
